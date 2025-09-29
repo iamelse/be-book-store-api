85b1c3c210d54267f69e5225b2279d25516eb818
Lana Septiana <lana.septiana2@gmail.com>
2025-09-29 11:10:15 +0700

    model, relation, and migration setup

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph(),
+            'price' => $this->faker->numberBetween(50000, 200000),
+            'isbn' => $this->faker->isbn13(),
+            'author' => $this->faker->name(),
+            'publisher' => $this->faker->company(),
+            'cover_image' => $this->faker->imageUrl(200,300,'books'),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User
+        \App\Models\User::factory()->create([
+            'name' => 'Demo User',
+            'email' => 'demo@example.com',
+            'password' => bcrypt('password'),
+            'access_key' => Str::random(40),
         ]);
+
+        // Buku nyata
+        $books = [
+            [
+                'title' => 'Clean Code',
+                'description' => 'A Handbook of Agile Software Craftsmanship',
+                'price' => 250000,
+                'isbn' => '9780132350884',
+                'author' => 'Robert C. Martin',
+                'publisher' => 'Prentice Hall',
+                'cover_image' => 'https://images-na.ssl-images-amazon.com/images/I/41xShlnTZTL._SX374_BO1,204,203,200_.jpg',
+            ],
+            [
+                'title' => 'Design Patterns: Elements of Reusable Object-Oriented Software',
+                'description' => 'Classic design patterns book',
+                'price' => 300000,
+                'isbn' => '9780201633610',
+                'author' => 'Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides',
+                'publisher' => 'Addison-Wesley',
+                'cover_image' => 'https://m.media-amazon.com/images/I/51kuc0iWo6L._SX376_BO1,204,203,200_.jpg',
+            ],
+            [
+                'title' => 'Refactoring: Improving the Design of Existing Code',
+                'description' => 'Martin Fowler\'s refactoring guide',
+                'price' => 280000,
+                'isbn' => '9780201485677',
+                'author' => 'Martin Fowler',
+                'publisher' => 'Addison-Wesley',
+                'cover_image' => 'https://m.media-amazon.com/images/I/41jEbK-jG+L._SX396_BO1,204,203,200_.jpg',
+            ],
+        ];
+
+        foreach ($books as $book) {
+            \App\Models\Product::create($book);
+        }
+
+        // Tambah dummy random products
+        \App\Models\Product::factory(10)->create();
     }
 }
